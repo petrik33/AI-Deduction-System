35d2d16d0311b7b09d8b6e7231d3bf96c5da2303
Timofey Petrikevich <petrik3003@gmail.com>
2023-11-24 10:42:42 +0300

    feat: implement a few rules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod properties_interface;
-mod rules;
-mod context;
-pub mod rules_impl;
-
 fn main() {
     // Use the modules and types here
 }
--- src/rules_impl.rs
+++ src/rules_impl.rs
@@ -5,6 +5,7 @@ use crate::properties::*;
 pub fn create_rules() -> Vec<Box<dyn Rule>> {
     let mut rules: Vec<Box<dyn Rule>> = Vec::new();
 
+    // Rule 1: If Body Temperature == High, Set Likelihood of Viral Infection to High
     let temp_rule: Box<dyn Rule> = Box::new(ProdRule {
         condition: Box::new(PropertyEqualsCondition {
             value: Property::BodyTemperature(PropBodyTemperature::High),
@@ -67,8 +68,51 @@ pub fn create_rules() -> Vec<Box<dyn Rule>> {
         outcome: Property::LikelihoodOfViralInfection(PropInfectionLikelihood::High),
     });
 
-
     rules.push(med_allergies_rule);
+
+    // Rule 6: If Body Temperature == High AND Pain Location == Abdomen AND Associated Symptoms include Nausea,
+    // Set Likelihood of Viral Infection to High (Consolidated from complex_rule_1)
+    // This rule overwrites complex_rule_1 and avoids conflict with Rule 3
+    let temp_pain_nausea_rule: Box<dyn Rule> = Box::new(ProdRule {
+        condition: Box::new(AndCondition {
+            condition1: Box::new(PropertyEqualsCondition {
+                value: Property::BodyTemperature(PropBodyTemperature::High),
+            }),
+            condition2: Box::new(AndCondition {
+                condition1: Box::new(PropertyEqualsCondition {
+                    value: Property::PainLocation(PropPainLocation::Abdomen),
+                }),
+                condition2: Box::new(PropertyEqualsCondition {
+                    value: Property::AssociatedSymptoms(PropAssociatedSymptopms::Nausea),
+                }),
+            }),
+        }),
+        outcome: Property::LikelihoodOfViralInfection(PropInfectionLikelihood::High),
+    });
+
+    rules.push(temp_pain_nausea_rule);
+
+    // Rule 7: If Age Group == Adult AND (Medical History == Diabetes OR Medical History == Hypertension),
+    // Set Likelihood of Viral Infection to Moderate (Consolidated from complex_rule_2)
+    // This rule overwrites complex_rule_2 and avoids conflict with Rule 1
+    let age_medical_history_rule: Box<dyn Rule> = Box::new(ProdRule {
+        condition: Box::new(AndCondition {
+            condition1: Box::new(PropertyEqualsCondition {
+                value: Property::AgeGroup(PropAgeGroup::Adult),
+            }),
+            condition2: Box::new(OrCondition {
+                condition1: Box::new(PropertyEqualsCondition {
+                    value: Property::MedicalHistory(PropMedicalHistory::Diabetes),
+                }),
+                condition2: Box::new(PropertyEqualsCondition {
+                    value: Property::MedicalHistory(PropMedicalHistory::Hypertension),
+                }),
+            }),
+        }),
+        outcome: Property::LikelihoodOfViralInfection(PropInfectionLikelihood::Moderate),
+    });
+
+    rules.push(age_medical_history_rule);
 
     rules
 }
